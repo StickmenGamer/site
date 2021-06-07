06b45c53dbf2cd9c4b8d44a14b0a433d7d72bd41
Baga <epileptickbot@gmail.com>
2021-06-07 15:49:56 +0300

    '123'

---
+++ assets/js/text.js
@@ -0,0 +1,28 @@
+$(function () {
+    var title = document.title,
+        animSeq = ["|"],
+        animIndex = 0,
+        titleIndex = 0;
+    function doInverseSpinZeroPitch() {
+        var loadTitle = title.substring(0, titleIndex);
+        if (titleIndex > title.length) {
+            animIndex = 0;
+            titleIndex = 0
+        }
+        if (animIndex > 1) {
+            titleIndex++;
+            animIndex = 0
+        }
+        document.title = loadTitle + animSeq[0];
+        animIndex++
+    }
+    window.setInterval(doInverseSpinZeroPitch, 150);
+});
+$(function () {
+    $(".typed").typed({
+        strings: ["Kto eto?", "Python developer", "Coder", "Music lover...", "MotherHacker", "Im clown", "Bruh", "Epileptick","D3m0"],
+        typeSpeed: 50,
+        loop: true,
+        cursorChar: "|",
+    });
+});
